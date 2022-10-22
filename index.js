dad786df8da7fa54e61bb1c21db14099db578dca
Valdemar_check <85429908+Valdemarcheck@users.noreply.github.com>
2022-10-22 15:49:55 +0500

    Setup validation system and responsive design for different screens

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-// Import all input elements
 // Add an event listener for each input
 // If button's input is invalid, put an explanation of why it is wrong
 // Make confirmation input invalid if it doesn't match password
-// Add event listener for submit button
-// Submit only if all buttons are valid
-// Otherwise don't allow submition
 
-const submit = document.getElementById("button[type='submit']")
-const first_name = document.getElementById("#first_name")
-const last_name = document.getElementById("#last_name")
-const email = document.getElementById("#email")
-const tel = document.getElementById("#phone_num")
-const password = document.getElementById("#password")
-const password_confirm = document.getElementById("#password_confirm")
+const first_name = document.querySelector("#first_name")
+const last_name = document.querySelector("#last_name")
+const email = document.querySelector("#email")
+const tel = document.querySelector("#phone_number")
+const password = document.querySelector("#password")
+const password_confirm = document.querySelector("#password_confirm")
+const form = document.querySelector('form');
+const inputs = [first_name, last_name, email, tel, password]
 
-const inputs = [first_name, last_name, email, tel, password, password_confirm]
+password_confirm.addEventListener('input', () => {
+    if (password_confirm.value != password.value) {
+        password_confirm.setCustomValidity('Your passwords don\'t match');
+        password_confirm.reportValidity();
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    const form_target = event.target;
+
+    if (form_target.checkValidity()) {
+        event.preventDefault();
+    }
+});
